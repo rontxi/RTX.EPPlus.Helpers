9dc0726ae8a5557440e327e06cb5f4cf2dd0f07d
Roger Amat <rontxi@gmail.com>
2018-03-08 18:05:42 +0100

    added Nuspec

--- RTX.EPPlus.Helpers/Properties/AssemblyInfo.cs
+++ RTX.EPPlus.Helpers/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RTX.EPPlus.Helpers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("EEPlus Library Helper")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RTX.EPPlus.Helpers")]
